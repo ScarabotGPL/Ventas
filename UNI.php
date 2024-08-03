b9aaacf8fab80691a6b6b31e754466d08eaf5c60
ScarabotGPL <87031658+ScarabotGPL@users.noreply.github.com>
2024-08-02 22:09:15 -0600

    Create UNI.php

---
+++ UNI.php
@@ -0,0 +1,16 @@
+<?php
+echo "<h1>Chedraui Andonegui</h1> <p><p><br>";
+echo "<center>";
+echo "<table>";
+echo "<caption>Ventas</caption>";
+echo "<td><a class='button' href='Dia.php'>Dia de Ayer</a></td>";
+echo "<td><a class='button' href='Indis.php'>Indispensables</a></td>";
+echo "<tr></tr>";
+echo "<td><a class='button' href='Mes.php'>Acumulada del Mes</a></td>";
+//echo "<td><a class='button' href='Indis2.php'>Indispensables Temp</a></td>";
+echo "<td><a class='button' href='MermaP.php'>Merma Perecederos</a></td>";
+echo "<tr></tr>";
+echo "<td><a class='button' href='Mesa.php'>Cierre Mes Anterior</a></td>";
+echo "<td><a class='button' href='Lugares.php'>Posiciones</a></td>";
+echo "<tr></tr>";
+?>
